0e51f2b871997cbe9fd9a67a21dbdc8ad0afe452
ccrossman <christopher.j.crossman@gmail.com>
2020-07-03 13:59:28 -0700

    Wire up CSS and JavaScript for todo.html

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,6 +53,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/todo")
+  String todo() {
+  	return "todo";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
