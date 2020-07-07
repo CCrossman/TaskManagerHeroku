fed7e169f12a60b8bcbee8cfe5b4345c328e0d32
ccrossman <christopher.j.crossman@gmail.com>
2020-07-07 15:11:19 -0700

    Added stub of login page.

---
+++ src/main/java/com/example/Auth.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.util.Objects;
+
+public final class Auth {
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Auth auth = (Auth) o;
+		return Objects.equals(getUsername(), auth.getUsername()) &&
+				Objects.equals(getPassword(), auth.getPassword());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getUsername(), getPassword());
+	}
+
+	@Override
+	public String toString() {
+		return "Auth{" +
+				"username='" + username + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -24,15 +24,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
 
 @Controller
 @SpringBootApplication
@@ -58,26 +55,40 @@ public class Main {
   	return "todo";
   }
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
+  @RequestMapping("/login")
+  String login() {
+  	return "login";
   }
+
+  @RequestMapping(value = "/login", method = {RequestMethod.POST, RequestMethod.PUT})
+  String login(@ModelAttribute Auth auth) {
+  	if (auth == null || auth.getUsername() == null || auth.getPassword() == null) {
+	    return "login";
+    }
+  	System.out.println("? " + auth);
+  	return "login";
+  }
+
+//  @RequestMapping("/db")
+//  String db(Map<String, Object> model) {
+//    try (Connection connection = dataSource.getConnection()) {
+//      Statement stmt = connection.createStatement();
+//      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//      ArrayList<String> output = new ArrayList<String>();
+//      while (rs.next()) {
+//        output.add("Read from DB: " + rs.getTimestamp("tick"));
+//      }
+//
+//      model.put("records", output);
+//      return "db";
+//    } catch (Exception e) {
+//      model.put("message", e.getMessage());
+//      return "error";
+//    }
+//  }
 
   @Bean
   public DataSource dataSource() throws SQLException {
