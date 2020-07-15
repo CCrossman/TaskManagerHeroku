b7bd88dff6dc18ae8a7741f151e54e4d4f8098dc
ccrossman <christopher.j.crossman@gmail.com>
2020-07-15 16:34:21 -0700

    Tweak to match Heroku db config

--- src/main/java/com/crossman/Main.java
+++ src/main/java/com/crossman/Main.java
@@ -74,12 +74,12 @@ public class Main {
 
 	@Bean
 	public DataSource dataSource() {
-		String db  = System.getenv("db-name");
+		String db  = System.getenv("DATABASE_URL");
 		String usr = System.getenv("db-username");
 		String pwd = System.getenv("db-password");
 
 		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl("jdbc:postgresql://localhost:5432/" + db + "?user=" + usr + "&password=" + pwd);
+		config.setJdbcUrl("jdbc:" + db + "?user=" + usr + "&password=" + pwd);
 		config.setDriverClassName("org.postgresql.Driver");
 		return new HikariDataSource(config);
 	}
