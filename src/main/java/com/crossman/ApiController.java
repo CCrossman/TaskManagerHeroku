a95e496c2ff15c0d2a828562b5e2bdf7f15b8c4a
ccrossman <christopher.j.crossman@gmail.com>
2020-08-02 20:04:04 -0700

    Added JWT Extension so that an old JWT can be upgraded to a fresh JWT behind the scenes, supporting long sessions.

--- src/main/java/com/crossman/ApiController.java
+++ src/main/java/com/crossman/ApiController.java
@@ -40,6 +40,29 @@ public class ApiController {
 	@Autowired
 	private UserLister userLister;
 
+	@RequestMapping(value = "/jwt/extend", method = RequestMethod.POST)
+	public String jwtExtend(HttpSession session, @RequestBody String jwt) {
+		final SecurityContext sc = getSecurityContext(session);
+		final String username = getUsernameFromSecurityContext(sc);
+		if (username == null) {
+			throw new IllegalStateException("username cannot be blank");
+		}
+
+		logger.debug("jwtExtend({},{})", username, jwt);
+		final DecodedJWT decodedJWT = JWT.decode(jwt);
+		final String tokenUsername = decodedJWT.getClaim("username").asString();
+		if (!username.equals(tokenUsername)) {
+			logger.debug("comparing {} to {}", username, tokenUsername);
+			throw new IllegalArgumentException("token must be owned by the session user");
+		}
+		final String issuer = decodedJWT.getIssuer();
+		if (!ISSUER.equals(issuer)) {
+			throw new IllegalArgumentException("invalid issuer '" + issuer + "'");
+		}
+		// if expired or not expired, create a new token
+		return createToken(sc, username);
+	}
+
 	@RequestMapping(value = "/jwt", method = RequestMethod.POST)
 	public Permissions jwtAuth(@RequestHeader("Authorization") String bearer) {
 		logger.debug(bearer);
@@ -52,11 +75,7 @@ public class ApiController {
 		final String username = decodedJWT.getClaim("username").asString();
 		final boolean isUser = decodedJWT.getClaim("user").asBoolean();
 		final boolean isAdmin = decodedJWT.getClaim("admin").asBoolean();
-
-		Date now = new Date();
-		if (now.before(decodedJWT.getNotBefore())) {
-			return new Permissions(username, "not before now", false, false);
-		}
+		final Date now = new Date();
 		if (decodedJWT.getExpiresAt().before(now)) {
 			return new Permissions(username, "expired token", false, false);
 		}
@@ -67,19 +86,12 @@ public class ApiController {
 	public String jwtCalculate(HttpSession session) {
 		final SecurityContext sc = getSecurityContext(session);
 		final String username = getUsernameFromSecurityContext(sc);
+
+		logger.debug("jwtCalculate({})", username);
 		if (username == null) {
 			throw new IllegalStateException("username cannot be blank");
 		}
-		final Collection<? extends GrantedAuthority> authorities = getAuthoritiesFromSecurityContext(sc);
-		final ZonedDateTime now = ZonedDateTime.now();
-		return JWT.create()
-				.withIssuer(ISSUER)
-				.withClaim("username", username)
-				.withClaim("user", authorities.contains(GrantedAuthorities.USER))
-				.withClaim("admin", authorities.contains(GrantedAuthorities.ADMIN))
-				.withExpiresAt(toDate(now.plusMinutes(15)))
-				.withNotBefore(toDate(now))
-				.sign(algorithm);
+		return createToken(sc, username);
 	}
 
 	@RequestMapping(value = "/promote/{username}", method = RequestMethod.POST)
@@ -140,6 +152,18 @@ public class ApiController {
 			return new ResponseEntity<>(new UserSummary(targetUsername, tasks), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+	}
+
+	private static String createToken(SecurityContext sc, String username) {
+		final Collection<? extends GrantedAuthority> authorities = getAuthoritiesFromSecurityContext(sc);
+		final ZonedDateTime now = ZonedDateTime.now();
+		return JWT.create()
+				.withIssuer(ISSUER)
+				.withClaim("username", username)
+				.withClaim("user", authorities.contains(GrantedAuthorities.USER))
+				.withClaim("admin", authorities.contains(GrantedAuthorities.ADMIN))
+				.withExpiresAt(toDate(now.plusMinutes(1)))
+				.sign(algorithm);
 	}
 
 	private static Date toDate(ZonedDateTime zdt) {
